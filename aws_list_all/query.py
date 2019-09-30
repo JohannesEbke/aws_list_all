1fdd14734e158fb5210692056d55ec29848ad23c
Ben Wart <benwart@users.noreply.github.com>
2019-09-30 11:14:04 -0700

    fixed exception when verbose mode enabled
    Services with no region don't render to console properly when run with verbosity enabled.

--- aws_list_all/query.py
+++ aws_list_all/query.py
@@ -203,7 +203,7 @@ def do_query(services, selected_regions=(), selected_operations=(), verbose=0, p
         for region in get_regions_for_service(service, selected_regions):
             for operation in get_listing_operations(service, region, selected_operations):
                 if verbose > 0:
-                    print('Service: {: <28} | Region: {:<15} | Operation: {}'.format(service, region, operation))
+                    print('Service: {: <28} | Region: {:<15} | Operation: {}'.format(service, (region,"(None)")[region is None], operation))
 
                 to_run.append([service, region, operation])
     shuffle(to_run)  # Distribute requests across endpoints
