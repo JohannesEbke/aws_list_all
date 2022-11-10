3967d7809264140d886485215b4a0317d9d10bd9
Johannes Ebke <ebke@hm.edu>
2022-11-11 00:31:09 +0100

    Include profile in client cache key

--- aws_list_all/client.py
+++ aws_list_all/client.py
@@ -5,6 +5,7 @@ _CLIENTS = {}
 
 def get_client(service, region=None, profile=None):
     """Return (cached) boto3 clients for this service and this region"""
-    if (service, region) not in _CLIENTS:
-        _CLIENTS[(service, region)] = boto3.Session(region_name=region, profile_name=profile).client(service)
-    return _CLIENTS[(service, region)]
+    key = (service, region, profile)
+    if key not in _CLIENTS:
+        _CLIENTS[key] = boto3.Session(region_name=region, profile_name=profile).client(service)
+    return _CLIENTS[key]
